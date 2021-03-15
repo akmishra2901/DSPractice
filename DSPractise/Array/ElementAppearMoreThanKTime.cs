4656593be7a7db1d2d46868290c502cda8c1153b
Ashish <MY_NAME@example.com>
2021-03-15 15:23:37 +0530

    Code To Find occurrence of element more than K [Input] times

---
+++ DSPractise/Array/ElementAppearMoreThanKTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+namespace DSPractise.Array
+{
+    // To Find 
+    public static class ElementAppearMoreThanKTime
+    {
+        public static int[] GetElementAppearsMoreThanKTime(this int[] inputArray, int k) {
+            List<int> retuval = new List<int>();
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+            foreach (int item in inputArray) {
+                if (keyValuePairs.ContainsKey(item))
+                    keyValuePairs[item] = keyValuePairs[item] + 1;
+                else
+                    keyValuePairs.Add(item, 1);
+            }
+            foreach (var keyValue in keyValuePairs) {
+                if (keyValue.Value > k)
+                    retuval.Add(keyValue.Key);
+            }
+            return retuval.ToArray();
+        }
+
+    }
+}
