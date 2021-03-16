7b5f42056de7e5bf5a02e609cf58cbc310905564
Ashish <MY_NAME@example.com>
2021-03-16 20:09:07 +0530

    Added new prblm solution.

---
+++ DSPractice/Arrays/ValidParentheses.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSPractice.Arrays
+{
+    public static class ValidParentheses
+    {
+        /*  Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+         *  determine if the input string is valid.
+         *  string inputArr = "(((((())))))";
+            bool result = inputArr.CheckValidParentheses();
+            Console.WriteLine(result);
+         */
+        public static bool CheckValidParentheses(this string inputStr) {
+            char[] strArr = inputStr.ToCharArray();
+            Dictionary<char, char> keyValuePairs = new Dictionary<char, char>();
+            keyValuePairs.Add(']', '[');
+            keyValuePairs.Add('}', '{');
+            keyValuePairs.Add(')', '(');
+            Stack<char> charStack = new Stack<char>();
+            for (int i = 0; i < strArr.Length; i++) {
+                char item = strArr[i];
+                if (keyValuePairs.ContainsKey(item))
+                {
+                    char popItem = charStack.Count > 0 ?  charStack.Pop() : '#';
+                    if (keyValuePairs[item] != popItem)
+                        return false;
+                }
+                else 
+                {
+                    charStack.Push(item);
+                }
+            }
+            return charStack.Count == 0;
+        }
+    }
+}
