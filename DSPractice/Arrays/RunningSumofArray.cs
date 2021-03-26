ce8d87f3deab9dd784eead7cd005ae0ba913870a
Ashish <MY_NAME@example.com>
2021-03-26 15:53:14 +0530

    Running Sum of 1D Array

---
+++ DSPractice/Arrays/RunningSumofArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSPractice.Arrays
+{
+    public static class RunningSumofArray
+    {
+        public static int[] RunningSum(this int[] nums)
+        {
+            if (nums.Length <= 1)
+            {
+                return nums;
+            }
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i - 1] + nums[i];
+            }
+            return nums;
+        }
+    }
+}
