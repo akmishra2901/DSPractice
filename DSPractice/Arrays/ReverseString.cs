ee6f30ee835334f7ba7a02a66c0823f77b9685ce
Ashish <MY_NAME@example.com>
2021-03-26 15:08:22 +0530

    Added New Programs

---
+++ DSPractice/Arrays/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSPractice.Arrays
+{
+    public static class ReverseCharArray
+    {
+        public static char[] GetReversedCharArray(this char[] inputArr) {
+            int start = 0;
+            int end = inputArr.Length - 1;
+            if (end == 0)
+                return inputArr;
+            while (start <= end) {
+                char temp = inputArr[start];
+                inputArr[start] = inputArr[end];
+                inputArr[end] = temp;
+            }
+            return inputArr;
+        }
+    }
+}
---
+++ DSPractice/Arrays/TwoNumSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSPractice.Arrays
+{
+    public static class TwoNumSum
+    {
+        public static int[] GetTwoNumbersPositionWhichHaveTargetSum(this int[] nums, int target) {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    result[0] = dic[nums[i]];
+                    result[1] = i;
+                }
+                else
+                {
+                    dic.Add(target - nums[i], i);
+                }
+            }
+            return result;
+        }
+    }
+}
